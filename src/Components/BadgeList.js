af2dc20895b79e3ad1707d7dcb6237ae8baf09b8
sabiqthottoly <sabiqt@gmail.com>
2021-03-31 04:36:08 +0400

    Create badgeList component and style it

---
+++ src/Components/BadgeList.js
@@ -0,0 +1,37 @@
+import React,{useState,useEffect} from 'react'
+import './BadgeList.css'
+
+
+function BadgeList(props) {
+    
+    useEffect(() => {
+        setCurrentIndex(null)
+    }, [props.title])
+    
+    const [currentIndex, setCurrentIndex] = useState(null)
+
+     return(
+         <div className='badge-contanier'>
+             <label>{props.title}</label>
+             <div className='badges-row'>
+                 { props.badgeList.map((item,index) => {
+                     return(
+                         <div 
+                            onClick={()=>{
+                                setCurrentIndex(index)
+                                props.onClick(item)
+                            }} 
+                            style={{backgroundColor: currentIndex === index ? "#8E27EA" : '#EFEFEF'}}
+                            className='badge-style'
+                         >
+                             <div style={{marginRight: 7}}>{item.emoji}</div>
+                             <div style={{color: currentIndex === index ? "#EFEFEF" : 'black'}}>{item.text}</div>
+                         </div>
+                     )
+                  })}
+             </div>
+         </div>
+     )
+}
+
+export default BadgeList
