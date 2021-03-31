b69a1c80e111c79ce30f220225dcb1607237a8d2
sabiqthottoly <sabiqt@gmail.com>
2021-03-31 04:35:05 +0400

    Create custom button and style it

---
+++ src/Components/CustomButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './CustomButton.css'
+
+function CustomButton(props){
+    return(
+        <div onClick={props.onClick} style={{width: "50%", height:60}}>
+            <div className={props.color === 'outline' ?'btn-outline' : 'btn-grad'} style={{display:'flex', justifyContent:'center'}}>
+                <span style={{fontSize: 17, marginTop: 3, marginBottom: 3}}>{props.children}</span>
+            </div>
+        </div>
+    )
+}
+
+export default CustomButton
