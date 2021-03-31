8db18868b268aef7d6d093cc76206f28566d1701
sabiqthottoly <sabiqt@gmail.com>
2021-03-31 14:15:18 +0400

    Documentation on Form file

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -6,39 +6,40 @@ import ProgressBar from '../Components/ProgressBar'
 import ResultComponent from '../Components/ResultComponent'
 import './styles/Form.css'
 
+//basicInfo data to show in the result page
+let BasicInfo = [
+    {
+        label : 'Name',
+        value : `${localStorage.getItem("firstname")} ${localStorage.getItem("lastname")}`
+    },
+    {
+        label : 'Date of birth',
+        value : localStorage.getItem("dateofbirth").split('00:00:00')[0],
+    },
+    {
+        label : 'Gender',
+        value : localStorage.getItem("gender"),
+    },
+    {
+        label : 'Nationality',
+        value : localStorage.getItem("nationality"),
+    },
+    {
+        label : 'Residence',
+        value : localStorage.getItem("residence"),
+    },
+    {
+        label : 'Contact Number',
+        value : localStorage.getItem("contactNumber"),
+    },
+    {
+        label : 'Email',
+        value : localStorage.getItem("email"),
+    },
+]
+
 function Form() {
-    
-    let BasicInfo = [
-        {
-            label : 'Name',
-            value : `${localStorage.getItem("firstname")} ${localStorage.getItem("lastname")}`
-        },
-        {
-            label : 'Date of birth',
-            value : localStorage.getItem("dateofbirth").split('00:00:00')[0],
-        },
-        {
-            label : 'Gender',
-            value : localStorage.getItem("gender"),
-        },
-        {
-            label : 'Nationality',
-            value : localStorage.getItem("nationality"),
-        },
-        {
-            label : 'Residence',
-            value : localStorage.getItem("residence"),
-        },
-        {
-            label : 'Contact Number',
-            value : localStorage.getItem("contactNumber"),
-        },
-        {
-            label : 'Email',
-            value : localStorage.getItem("email"),
-        },
-    ]
-    
+    //States
     const [formContent, setFormContent] = useState([])
     const [formIndex, setFormIndex] = useState(1)
     const [progessValue, setProgessValue] = useState(1)
@@ -46,10 +47,12 @@ function Form() {
     const [questionnaire, setQuestionnaire] = useState([])
     const [editMode, setEditMode] = useState(false)
 
+    
     useEffect(() => {
         getData()    
     }, [])
 
+    //function  to fetch contentList data from json file
     const getData = () => {
         fetch('data.json'
         ,{
@@ -68,18 +71,19 @@ function Form() {
           });
     }
 
+    //function to handle next button 
     function nextButtonHandler() {
-        if (editMode) {
+        if (editMode) {                         // next button onclick if user entering after clicking edit icon in result page
             setFormIndex(9)
             setEditMode(false)
             setProgessValue(8)
-        } else if (formIndex === 1 && formCurrentValue && formCurrentValue.length >= 3 ) {
+        } else if (formIndex === 1 && formCurrentValue && formCurrentValue.length >= 3 ) { // next button handling for first three content, since all three in one screen 
             localStorage.setItem(formIndex, JSON.stringify(formCurrentValue));
             setQuestionnaire([...questionnaire, ...formCurrentValue])
             setFormIndex(formIndex + 2)
             setFormCurrentValue([])
             setProgessValue(progessValue+1)
-        } else if (formIndex > 2 && formCurrentValue.length != 0) {
+        } else if (formIndex > 2 && formCurrentValue.length != 0) { // next button handling for contents above 3 , since above three all are content per screen 
             setQuestionnaire([...questionnaire,...formCurrentValue])
             localStorage.setItem(formIndex, JSON.stringify(formCurrentValue));
             setFormIndex(formIndex + 1)
@@ -88,13 +92,14 @@ function Form() {
         }
     }
 
-    function backButtonHandler() {
-        if(formIndex === 3){
+    //function to handel backbutton 
+    function backButtonHandler() { 
+        if (formIndex === 3) { //backbutton handler for first 3 
             let removelastelement = questionnaire.slice(0, questionnaire.length - 3)
             setQuestionnaire(removelastelement)
             setFormIndex(1)
             setProgessValue(progessValue-1)
-        } else {
+        } else if (formIndex > 0 ) {              //backbutton handler for rest and check if index lessthan zero 
             let removelastelement = questionnaire.slice(0, questionnaire.length - 1)
             setQuestionnaire(removelastelement)
             setFormIndex(formIndex - 1)
@@ -102,7 +107,8 @@ function Form() {
         }
         setFormCurrentValue([])
     }
-
+    
+    //function to handle on clicking the edit icon in the result page
     function questionnaireEditHandler(index) {
         setEditMode(true)
         setFormIndex(index)
@@ -113,11 +119,13 @@ function Form() {
         }
     }
 
+    //function to handle the onClick on the badge details if user entering via clicking edit icon from resultpage
     function EditModeBadgeOnClickHandler(badgeValue, title){
-        let editIndex = null
-        questionnaire.forEach((item,index)=> {
+        let editIndex = null    
+
+        //code to replace the exisiting value in the questionnair array for editting new value
+        questionnaire.forEach((item,index)=> {  
             if (item.title === title) {
-                console.log("true")
                  editIndex = index
             }
         })
@@ -129,10 +137,11 @@ function Form() {
 
     }
 
+    //function to handle when user click the badge to give values
     function badgeOnClickHandler(badgeValue, title) {
-        if(formIndex === 1){
+        if(formIndex === 1) { 
             
-            formCurrentValue.forEach((item,index) => {
+            formCurrentValue.forEach((item,index) => { // loop and checking if the user changes the badge values again, if yes replace it(since all three content in one screen)
                 if(item.title === title) {
                     formCurrentValue.splice(index,1)
                 }     
@@ -150,7 +159,7 @@ function Form() {
                 <Col xs='0' xl='6' md='6' style={{fontSize: 35, fontWeight:'bold'}}>digg</Col>
                 <Col xs='12' xl='6' md='6'>
                     <ProgressBar
-                        width={`${(progessValue/8)*100}%`}
+                        width={`${(progessValue/8)*100}%`} 
                     />
                 </Col>
             </Row>
@@ -198,7 +207,7 @@ function Form() {
                 }
             <Row xl='6' style={{marginTop: 30}}>
                 <CustomButton color='outline' onClick={() => backButtonHandler() }>Back</CustomButton>
-                <CustomButton onClick={() => nextButtonHandler() }>{formIndex >= 9 ? 'Compelete' :'Next'}</CustomButton>
+                <CustomButton onClick={() => nextButtonHandler() }>{formIndex >= 9 || editMode ? 'Compelete' :'Next'}</CustomButton>
             </Row>
         </Container >
 
